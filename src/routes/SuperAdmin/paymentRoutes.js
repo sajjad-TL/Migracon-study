2dab6584d03603ebfe38dc1d363fce848c03d0cf
Shakeel112906 <shakeel.sakha@tecklogics.com>
2025-06-20 11:29:29 -0400

    agent in a payment and search query section

--- src/routes/SuperAdmin/paymentRoutes.js
+++ src/routes/SuperAdmin/paymentRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createPayment, getPaymentsByAgent, getLatestPaymentByAgent, getAllPayments } = require('../../controllers/SuperAdmin/paymentController')
+const { createPayment, getPaymentsByAgent, getLatestPaymentByAgent, getAllPayments, updatePaymentStatus, deletePayment } = require('../../controllers/SuperAdmin/paymentController')
 
 router.post('/create', createPayment);
 router.get('/by-agent/:agentId', getPaymentsByAgent);
 router.get('/summary/:agentId', getLatestPaymentByAgent);
 router.get('/getAllPayments', getAllPayments);
+router.patch('/update/:paymentId', updatePaymentStatus);
+router.delete('/delete/:paymentId', deletePayment);
+
 
 module.exports = router;
