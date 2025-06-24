8ab1d7cba8bea2876dd529b43345b0503f838aa7
qasim-teck <qasimtecklogics@gmail.com>
2025-06-24 18:19:58 +0500

    some changes

--- src/routes/SuperAdmin/reportsRoutes.js
+++ src/routes/SuperAdmin/reportsRoutes.js
@@ -1,21 +1,13 @@
-// src/routes/SuperAdmin/commission.js
 const express = require("express");
 const router = express.Router();
 // const reportController = require("../../controllers/SuperAdmin/reportController");
-const { createReport , getReports, getReportTrends, exportExcelReport } = require('../../controllers/SuperAdmin/reportController')
+const { createReport , getReports, getReportTrends } = require('../../controllers/SuperAdmin/reportController')
 
-// GET /api/commission/dashboard/stats
-router.get("/dashboard/stats", getDashboardStats);
-
-// GET /api/commission/agents
-router.get("/agents", getAgents);
+// POST /api/reports
+router.post("/createReport", createReport);
 
 // GET /api/reports?months=6
 router.get("/getReport", getReports);
 router.get('/trends', getReportTrends);
-router.get('/exportExcel', exportExcelReport);
-
-// GET /api/commission/export
-router.get("/export", exportCommissionData);
 
 module.exports = router;
